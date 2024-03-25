75948255b2a8dd64da5aeb655257ef50589f62f3
Pawn3r <anduu150620@gmail.com>
2024-03-25 13:56:53 +0700

    first commit

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientSocket
+{
+    internal class Program
+    {
+        private static string msg;
+
+        static void Main(string[] args)
+        {
+            IPAddress server_ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipe = new IPEndPoint(server_ip, 1234);
+            Socket cs = new Socket(server_ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            cs.Connect(ipe);
+            byte[] b = new byte[1024];
+            Array.Clear(b, 0, b.Length);
+            cs.Receive(b);
+            msg = Encoding.ASCII.GetString(b).TrimEnd('\0');
+            Console.WriteLine("[+] Received from server: {0}", msg);
+            cs.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServerSocket/ServerSocket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 1234);
+            // IPAddress.Any == 0.0.0.0
+            Socket server_socket = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            server_socket.Bind(ipe);
+            server_socket.Listen(5);
+
+            Socket client_socket = server_socket.Accept();
+            Console.WriteLine("[+] Get Connection from: {0}", client_socket.LocalEndPoint);
+            Console.WriteLine("Enter message to send: ");
+            string msg = Console.ReadLine();
+            client_socket.Send(Encoding.ASCII.GetBytes(msg));
+            client_socket.Close();
+            server_socket.Close();
+            Console.ReadKey();
+
+        }
+
+    }
+}
